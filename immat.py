c41fe997da33b15f2ad3c1174954471119ad07bd
Arthurmrtl <34254490+Arthurmrtl@users.noreply.github.com>
2021-07-09 14:09:30 +0200

    Sauvegarde des voiture
    Possibilité d'enregistrer et de sauvegarder plusieurs voitures.

--- immat.py
+++ immat.py
@@ -1,14 +1,33 @@
 import pip._vendor.requests
 import time
+from os import walk
+path = '/data/data/com.termux/files/home/runoupas'
 
 
 print('+-+-+-+ +-+-+ +-+-+-+\n|R|U|N| |O|U| |P|A|S|\n+-+-+-+ +-+-+ +-+-+-+\n')
-print("1. Est-ce que je le fume ?")
-print("2. Enregistrer ma voiture")
+
+filenames = next(walk(path), (None, None, []))[2]
+print('Tu roules en quoi ?')
+i = 1
+for car in filenames:
+    print(str(i) + '. ' + car)
+    i = i + 1
+print(str(i) + '. ' + 'Ajouter une caisse')
 choice = input()
 
-
-if int(choice) == 1:
+if int(choice) == i:
+    print("Quel est ta plaque d'immatriculation ? (AA111BB)")
+    plate = input()
+    r=pip._vendor.requests.get('https://www.yakarouler.com/car_search/immat?immat=' + plate + '&name=undefined&redirect=true')
+    data = r.text
+    finder_start = data.find('<h2 class="title-car">')
+    car = data[finder_start+22:data.find('</h2>')]
+    model = car[:car.find('(')]
+    rpath =  path + "/" +  model
+    open(rpath, 'w').close()
+    f = open(rpath, "w")
+    f.write(car)
+elif int(choice) < i:
     print("Quel est la plaque d'immatriculation ? (AA111BB)")
     plate = input()
     r=pip._vendor.requests.get('https://www.yakarouler.com/car_search/immat?immat=' + plate + '&name=undefined&redirect=true')
@@ -25,7 +44,7 @@ if int(choice) == 1:
         elif 'cv' in spec:
             hp = spec
     print("\nC'est un(e) " + model + "de " + hp)
-    f = open("data", "r")
+    f = open(path + "/" + filenames[int(choice)-1], "r")
     mycar = f.read()
     for spec in mycar.split():
          if 'cv' in spec:
@@ -51,14 +70,5 @@ if int(choice) == 1:
         print(' | |_| | |_| | |_| / ___ \| |_) | |___| |___ ')
         print('  \___/ \___/ \___/_/   \_\____/|_____|_____|')
     time.sleep(60)
-
-if int(choice) == 2:
-    print("Quel est ta plaque d'immatriculation ? (AA111BB)")
-    plate = input()
-    r=pip._vendor.requests.get('https://www.yakarouler.com/car_search/immat?immat=' + plate + '&name=undefined&redirect=true')
-    data = r.text
-    finder_start = data.find('<h2 class="title-car">')
-    car = data[finder_start+22:data.find('</h2>')]
-    open('data', 'w').close()
-    f = open("data", "w")
-    f.write(car)
+else:
+    print('error')
